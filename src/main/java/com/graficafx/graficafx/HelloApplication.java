58d0ab23cc66e30cbbb9af7e84fa17eb5345d3d6
fran <franperezthiar@gmail.com>
2023-12-12 11:54:22 +0100

    Add

--- src/main/java/com/graficafx/graficafx/HelloApplication.java
+++ src/main/java/com/graficafx/graficafx/HelloApplication.java
@@ -1,8 +1,12 @@
 package com.graficafx.graficafx;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +15,19 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 610, 460);
         stage.setTitle("Gráficas en JavaFX");
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                        new PieChart.Data("Grapefruit", 13),
+                        new PieChart.Data("Oranges", 25),
+                        new PieChart.Data("Plums", 10),
+                        new PieChart.Data("Pears", 22),
+                        new PieChart.Data("Apples", 30));
+        final PieChart chart = new PieChart(pieChartData);
+        chart.setTitle("Imported Fruits");
+
+        ((Group) scene.getRoot()).getChildren().add(chart);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/graficafx/graficafx/HelloController.java
+++ src/main/java/com/graficafx/graficafx/HelloController.java
@@ -1,6 +1,10 @@
 package com.graficafx.graficafx;
 
+import javafx.beans.binding.Bindings;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.LineChart;
@@ -8,36 +12,30 @@ import javafx.scene.chart.PieChart;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.AnchorPane;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     @FXML
-    private AnchorPane barpane;
-
-    @FXML
-    private AnchorPane linepane;
-
-    @FXML
-    private AnchorPane piepane;
-
-    @FXML
-    private Tab tabbar;
-
-    @FXML
-    private Tab tabline;
-
-    @FXML
-    private Tab tabpie;
-
-    @FXML
-    private BarChart<?, ?> mybarchart;
-
-    @FXML
-    private LineChart<?, ?> mylinechart;
+    private AnchorPane pane;
 
     @FXML
     private PieChart mypiechart;
 
 
-
-
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                        new PieChart.Data("Java",23),
+                        new PieChart.Data("JavaScript", 34),
+                        new PieChart.Data("C#",43));
+        pieChartData.forEach(data -> data.nameProperty().bind(
+                Bindings.concat(
+                        data.getName(),"Cantidad",data.pieValueProperty()
+                )
+        ));
+        mypiechart.getData().addAll(pieChartData);
+    }
 }
